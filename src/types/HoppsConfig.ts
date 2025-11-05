43571effd54ad6fe8028711bdfded8bd024d45da
Denis Zhmurenko <hi@dyzoon.dev>
2025-11-05 17:38:12 +0000

    feat: ability to control the requeue on error behaviour (#1)
    * feat: ability to control the requeue on error behaviour
    
    * remove connection url from logs

--- src/types/HoppsConfig.ts
+++ src/types/HoppsConfig.ts
@@ -11,6 +11,16 @@ export type HoppsConfig<T extends readonly string[]> = {
     /** Queues to send messages to */
     outboundQueues?: T;
     /**
+     * Whether to requeue a message when an unhandled error in a consumer is caught
+     *
+     * Acts as a global setting, can be rewritten in each of the {@link Queue}s
+     *
+     * More info on the requeue in {@link https://amqp-node.github.io/amqplib/channel_api.html#channel_nack|the amqplib doc}
+     *
+     * @default true
+     */
+    requeueOnError?: boolean;
+    /**
      * Whether to consume from the Direct Reply-To queue for RPC pattern
      * @default false
      */
